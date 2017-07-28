ba153f0ac0ab3cfe31ad9411ec76421a4714dee3
Scott Weiss <sdw35@cornell.edu>
2017-07-28 11:39:15 -0400

    switch back to hostonly network master is an etcd and a node add labels and tags to properly give public urls the right way

--- add-ssh-keys.sh
+++ add-ssh-keys.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 ssh-add -D
+rm -f ~/.ssh/known_hosts
 for i in $(ls vagrant/.vagrant/machines); do ssh-add vagrant/.vagrant/machines/$i/virtualbox/private_key ; done
 for i in $(cat ips); do ssh-keyscan $i >> ~/.ssh/known_hosts; done
--- gen_hosts_file.sh
+++ gen_hosts_file.sh
@@ -12,7 +12,7 @@ get_ip() {
 
 # for virtualbox
 get_ip_via_ssh() {
-	local ipinfo=$(vagrant ssh $1 -- /usr/sbin/ip addr | grep inet | grep 172.31) #192.168)
+	local ipinfo=$(vagrant ssh $1 -- /usr/sbin/ip addr | grep inet | grep 192.168)
     local ip=$(echo $ipinfo | sed -r 's/.*([0-9][0-9][0-9]+\.[0-9]+\.[0-9]+\.[0-9]+)\/.*/\1/')
     echo ${ip}
 }
@@ -26,26 +26,28 @@ etcd
 
 [OSEv3:vars]
 ansible_ssh_user=vagrant
+ansible_become=true
 deployment_type=origin
+openshift_disable_check=disk_availability,memory_availability,docker_storage,package_version
 
 [masters]
 EOF
 host=$(get_ip_via_ssh master)
-echo $host >> ${TMPHOSTSFILE}
+echo "${host} openshift_schedulable=true openshift_ip=${host} openshift_public_ip=${host} openshift_public_hostname=${host} openshift_hostname=${host}" >> ${TMPHOSTSFILE}
 echo $host > ${IPS}
 echo "" >> ${TMPHOSTSFILE}
+echo [etcd] >> ${TMPHOSTSFILE}
+echo "${host} openshift_schedulable=true openshift_ip=${host} openshift_public_ip=${host} openshift_public_hostname=${host} openshift_hostname=${host}" >> ${TMPHOSTSFILE}
+echo "" >> ${TMPHOSTSFILE}
 echo [nodes] >> ${TMPHOSTSFILE}
-nodes=$(cat cluster.yml | python /usr/lib/python2.7/site-packages/yq/__main__.py .nodes) && \
+host=$(get_ip_via_ssh master)
+echo "${host} openshift_schedulable=true openshift_ip=${host} openshift_public_ip=${host} openshift_public_hostname=${host} openshift_hostname=${host} openshift_node_labels=\"{'region': 'infra'}\" " >> ${TMPHOSTSFILE}
+nodes=$(cat cluster.yml | python /usr/lib/python2.7/site-packages/yq/__main__.py .nodes)
 for i in $(seq 1 ${nodes}); do
     host=$(get_ip_via_ssh node${i})
-    echo $host >> ${TMPHOSTSFILE}
+    echo "${host} openshift_schedulable=true openshift_ip=${host} openshift_public_ip=${host} openshift_public_hostname=${host} openshift_hostname=${host} openshift_node_labels=\"{'region': 'infra'}\" " >> ${TMPHOSTSFILE}
     echo $host >> ${IPS}
-done && \
-echo "" >> ${TMPHOSTSFILE}
-echo [etcd] >> ${TMPHOSTSFILE}
-host=$(get_ip_via_ssh etcd)
-echo $host >> ${TMPHOSTSFILE}
-echo $host >> ${IPS}
-cat ${TMPHOSTSFILE} && \
-rm ${TMPHOSTSFILE} && \
+done
+cat ${TMPHOSTSFILE}
+rm ${TMPHOSTSFILE}
 cd ..
